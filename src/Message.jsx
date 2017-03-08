4a14ac1b660eee330c146ee7608a79dbb4084d15
Kirat Virk <kirat_virk3@yahoo.ca>
2017-03-08 23:50:01 +0000

    added image displaying

--- src/Message.jsx
+++ src/Message.jsx
@@ -5,10 +5,28 @@ class Message extends Component {
   render() {
     console.log("Rendering <Message/>");
     let style = {color: this.props.color}
+    let urlReg = /(https?:\/\/[^\s]+(.jpg|.png))/g;
+    let match = urlReg.exec(this.props.content);
+    let content = this.props.content.replace(urlReg, "");
+    let url = "";
+    if (match !== null){
+      url = match[0];
+    }
+    if(url === ""){
+      return (
+          <div className="message">
+            <span style={style} className="message-username">{this.props.username}</span>
+            <span className="message-content">{content}</span>
+          </div>
+      );
+    }
     return (
         <div className="message">
           <span style={style} className="message-username">{this.props.username}</span>
-          <span className="message-content">{this.props.content}</span>
+          <div className="img">
+            <span className="message-content">{content}</span>
+            <img src={url}/>
+          </div>
         </div>
     );
   }
